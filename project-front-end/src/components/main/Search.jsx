a0bdcad90d1c05a42783052d0cf4dbeef27777c7
eore <eore26@gmail.com>
2018-03-07 02:10:10 +0700

    update

--- project-front-end/src/components/main/Search.jsx
+++ project-front-end/src/components/main/Search.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Product from './product/Product';
 import products from '../../products.json';
 
-let search = keyword => products.filter(x => (new RegExp(keyword, 'gi').test(x.name))).map(x => <Product key={x.id} {...x}/>)
+let search = keyword => {
+    let result = products.filter(x => (new RegExp(keyword, 'gi').test(x.name))).map(x => <Product key={x.id} {...x}/>)
+    if (result.length === 0) return <h1>Oops, menu sedang di masak</h1>
+    else return result;
+}
 
 const Search = props =>
     <div className="display-product">
